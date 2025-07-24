8336f445d04a965dbff99f312e6fd0e4a92cb8b7
Sanghita Seal <sealsanghita2005@gmail.com>
2025-07-24 15:03:24 +0530

    Done only the UI part of the password generator

---
+++ 05passwordgenerator/src/App.jsx
@@ -0,0 +1,71 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('')
+
+
+  
+  return (
+    <>
+     <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input
+         type="text"
+          value={password}
+          className='outline-none w-full py-1 px-3 bg-gray-400 text-black'
+          placeholder='Password'
+          readOnly
+
+        />
+        <button
+       
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >Copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+        <div className='flex items-center gap-x-1'>
+            <input 
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='cursor-pointer  accent-blue-600'
+              onChange={(e)=>{setLength(Number(e.target.value))}}
+            />
+            <label htmlFor="length">Length: {length}</label>
+        </div>
+        <div className='flex items-center gap-x-1  accent-blue-600'>
+           <input 
+              type="checkbox" 
+              defaultChecked={numberAllowed}
+              onChange={()=>{
+                setNumberAllowed((prev)=> !prev)
+              }}
+           />
+          <label htmlFor="number">Numbers </label>
+
+        </div>
+        <div className='flex items-center gap-x-1  accent-blue-600'>
+           <input 
+              type="checkbox" 
+              defaultChecked={charAllowed}
+              onChange={()=>{
+                setNumberAllowed((prev)=> !prev)
+              }}
+           />
+          <label htmlFor="charInput">Characters </label>
+
+        </div>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
